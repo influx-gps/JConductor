4e286d04f13da1c99d3461f593e6d0226da00870
AdamGrzybkowski <agrzybkowski@outlook.com>
2016-10-15 10:52:35 +0200

    Added account endpoint plus some Unit tests

---
+++ src/main/java/gut/follower/org/Controller/AccountController.java
@@ -0,0 +1,66 @@
+package gut.follower.org.Controller;
+
+import gut.follower.org.Models.Account;
+import gut.follower.org.Repositories.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/rest/account")
+public class AccountController {
+
+    @Autowired
+    AccountRepository repository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Account getAccount(Principal principal){
+        return Optional
+                .ofNullable(repository.findByUsername(principal.getName()))
+                .orElseThrow(() -> new IllegalStateException("Account does not exist"));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Account updateAccount(Principal principal, @RequestBody Account account){
+        return Optional
+                .ofNullable(account)
+                .map(newAcc -> {
+                    Account oldAcc = getAccountByName(principal);
+                    oldAcc.setPassword(getPassword(oldAcc, newAcc));
+                    oldAcc.setEmail(getEmail(oldAcc, newAcc));
+                    return repository.save(oldAcc);
+                })
+                .orElseThrow(() -> new IllegalStateException("Changes could not be applied"));
+    }
+
+    private String getEmail(Account oldAcc, Account newAcc) {
+        return Optional
+                .ofNullable(newAcc.getEmail())
+                .map(this::getNewEmailIfNotAlreadyTaken)
+                .orElse(oldAcc.getEmail());
+    }
+
+    private String getNewEmailIfNotAlreadyTaken(String email) {
+        return Optional
+                .ofNullable(email)
+                .filter(em -> repository.findByEmail(em) == null)
+                .orElseThrow(() -> new IllegalStateException("Email already taken"));
+    }
+
+    private String getPassword(Account oldAcc, Account newAcc) {
+        return Optional
+                .ofNullable(newAcc.getPassword())
+                .orElse(oldAcc.getPassword());
+    }
+
+    private Account getAccountByName(Principal principal) {
+        return Optional
+                .ofNullable(repository.findByUsername(principal.getName()))
+                .orElseThrow(() -> new IllegalStateException("That account does not exist"));
+    }
+}
---
+++ src/test/java/gut/follower/org/ControllerTests/AccountControllerTest.java
@@ -0,0 +1,64 @@
+package gut.follower.org.ControllerTests;
+
+import gut.follower.org.Controller.AccountController;
+import gut.follower.org.Controller.RegisterController;
+import gut.follower.org.Models.Account;
+import gut.follower.org.Repositories.AccountRepository;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.security.Principal;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AccountControllerTest {
+
+    private static final String TEST_EMAIL = "test@example.com";
+    private static final String TEST_USERNAME = "testUsername";
+    private static final String TEST_PASSWORD = "testPassword";
+    private static final String TEST_PASSWORD_MOD = "testPasswordMod";
+    private static final Account acc = new Account(TEST_USERNAME, TEST_PASSWORD, TEST_EMAIL);
+    private static final Account acc_mod = new Account(TEST_USERNAME, TEST_PASSWORD_MOD, TEST_EMAIL);
+
+    @Mock
+    private AccountRepository accountRepository;
+
+    @InjectMocks
+    private AccountController sut;
+
+    @Mock
+    private Principal principal;
+
+    @Before
+    public void setUp() {
+        when(accountRepository.findByEmail(acc.getEmail())).thenReturn(null);
+        when(accountRepository.findByUsername(acc.getUsername())).thenReturn(acc);
+        when(accountRepository.save(acc)).thenReturn(acc_mod);
+        when(principal.getName()).thenReturn(TEST_USERNAME);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testGetAccount_accDoesNotExist(){
+        when(accountRepository.findByUsername(TEST_USERNAME)).thenReturn(null);
+        sut.getAccount(principal);
+    }
+
+    @Test
+    public void testUpdateAccount_allOk() {
+        Account result = sut.updateAccount(principal, acc);
+        Assert.assertEquals(acc_mod, result);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testUpdateAccount_emailTaken() {
+        when(accountRepository.findByEmail(acc.getEmail())).thenReturn(acc);
+        sut.updateAccount(principal, acc);
+    }
+
+}
