a4e85ca001714654c030f3588e4814733748636c
AdamGrzybkowski <AdamGrzybkowski@users.noreply.github.com>
2016-10-16 20:23:33 +0200

    Added account endpoint plus some Unit tests (#5)
    * Added account endpoint plus some Unit tests
    
    * set track distance to 0 in constructor

--- src/test/java/gut/follower/org/JConductorApplicationTests.java
+++ src/test/java/gut/follower/org/JConductorApplicationTests.java
@@ -3,11 +3,12 @@ package gut.follower.org;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = JConductorApplication.class)
+@SpringBootTest(classes = JConductorApplication.class)
 @WebAppConfiguration
 public class JConductorApplicationTests {
 
