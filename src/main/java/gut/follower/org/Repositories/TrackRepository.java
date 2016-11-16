2a0a231ac46061ef761616ed0c75692a7d2a1bfb
AdamGrzybkowski <agrzybkowski@outlook.com>
2016-11-16 08:31:33 +0100

    Returning tracks without locations

--- src/main/java/gut/follower/org/Repositories/TrackRepository.java
+++ src/main/java/gut/follower/org/Repositories/TrackRepository.java
@@ -2,10 +2,12 @@ package gut.follower.org.Repositories;
 
 import gut.follower.org.Models.Track;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface TrackRepository extends MongoRepository<Track, String>{
     public Track findByAccountIdAndId(String accountId, String Id);
+    @Query(fields = "{'locations' : 0}")
     public List<Track> findByAccountIdOrderByStartTimeDesc(String accountId);
 }
